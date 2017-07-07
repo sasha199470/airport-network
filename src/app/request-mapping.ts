28d7817b85b403e3fe7e2d772bbb5750c6f9946b
Alexandra Andreeva <alexandra.andreeva.korbit@gmail.com>
2017-07-07 19:46:31 +0200

    Url запросов

--- src/app/request-mapping.ts
+++ src/app/request-mapping.ts
@@ -2,9 +2,30 @@
  * Created by alexandra on 7/5/17.
  */
 export abstract class RequestMapping {
-  private static commonUrl = 'https://api.flightstats.com/flex';
-  private static keyId = '?appId=2f3d54f5&appKey=6490a6a02e6b8f094a2a5baca024fec2';
-  // private static appInfo = {'appId': '2f3d54f5', 'appKey': '6490a6a02e6b8f094a2a5baca024fec2'};
-  static airports = RequestMapping.commonUrl + '/airports/rest/v1/json/countryCode/RU' + RequestMapping.keyId;
-  static schedules = RequestMapping.commonUrl + '/schedules/rest/v1/json/from/{departureAirportCode}/departing/{year}/{month}/{day}/{hourOfDay}' + RequestMapping.keyId;
+
+  private static readonly commonUrl = '/flex';
+
+  private static readonly appId = '2f3d54f5';
+  private static readonly appKey = '6490a6a02e6b8f094a2a5baca024fec2';
+  private static readonly appParams = '?appId=' + RequestMapping.appId +
+                                      '&appKey=' + RequestMapping.appKey;
+
+  private static readonly countryCode = 'RU';
+  public static readonly airports = RequestMapping.commonUrl +
+                                    '/airports/rest/v1/json/countryCode/' +
+                                    RequestMapping.countryCode +
+                                    RequestMapping.appParams;
+
+  private static readonly schedules = RequestMapping.commonUrl + '/schedules/rest/v1/json/from/';
+  public static getFlight(departureAirportFS: string,
+                   date: Date) : string {
+    let path = [departureAirportFS,
+                'departing',
+                date.getUTCFullYear(),
+                date.getMonth() + 1,
+                date.getDate(),
+                date.getHours()].join('/');
+    let searchParam = '&codeType=FS';
+    return this.schedules + path + RequestMapping.appParams + searchParam;
+  }
 }
